f499d71e179892df6260e0c53df576e7ab8961c4
KHOA-PCKhoa <lakhoa1989@gmail.com>
2019-01-16 23:45:46 +0700

    complete add Zara ES

--- moon.js
+++ moon.js
@@ -436,7 +436,7 @@ const WEBSITES = {
     TAX: 0,
     RATE: 30000,
     MATCH: "zara.com/es",
-    JSONBLOCK: "$.offers[0].price"
+    JSONBLOCK: "$[0].offers.price"
   },
   ZULILY: {
     TAX: 0,
