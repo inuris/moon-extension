a08b2e814a6ea7d145fadb36e638592c7d47e0f2
Khoa Lam <lakhoa1989@gmail.com>
2024-05-29 14:22:21 +0700

    update manifest v3

--- background.js
+++ background.js
@@ -1,22 +1,16 @@
 
 // Listen for when a Tab changes state
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
-    if(changeInfo && changeInfo.status === "complete"){
+
+chrome.tabs.onUpdated.addListener(async function (tabId, changeInfo, tab) {
+    if (changeInfo && changeInfo.status === "complete" && tab) {
         console.log("Tab updated: " + tab.url);
-        setTimeout(function(){
-            chrome.tabs.sendMessage(tabId, {method: "changeTab" }, function(response) {
+        try {
+            await chrome.tabs.sendMessage(tabId, { method: "changeTab" }, function (response) {
                 console.log(response);
             });
-        },1000)
-        
+        }
+        catch (e) {
+            console.log(e);
+        }
     }
-});
-
-// So sánh version, nếu có mới thì nhảy ra thông báo
-var manifest = chrome.runtime.getManifest();
-if (localStorage.version != manifest.version) {
-    chrome.browserAction.onClicked.addListener(function () {
-        localStorage.version = manifest.version;
-        alert("Công cụ báo giá MoonHangMy vừa được cập nhật. Nếu công cụ bị ngưng hoạt động, vui lòng vào Chrome Menu -> Chọn công cụ MoonHangMy ở cuối -> Re-enable.")
-    });
-}
+})
