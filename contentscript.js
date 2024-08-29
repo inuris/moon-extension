4e38932c19426f283433331b604c40b5b064c1b6
Khoa Lam <lakhoa1989@gmail.com>
2024-08-29 09:27:15 +0700

    update rate

--- contentscript.js
+++ contentscript.js
@@ -12,7 +12,7 @@ s.onload = function () {
 	s.remove();
 };
 
-const RATE_USD_VND = 26200;
+const RATE_USD_VND = 25500;
 const FACEBOOK_APPID = '878760088950034';
 const FACEBOOK_PAGEID = '573537602700846';
 const FACEBOOK_LINK = 'https://www.facebook.com/moonhangmy';
